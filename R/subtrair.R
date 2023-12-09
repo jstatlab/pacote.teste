f52429941beb3681f16447775e35ea0147d66fca
jstatlab <jstatlab@gmail.com>
2023-12-09 09:45:43 -0300

    Criei o arquivo subtrair.R

---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#' subtrair return the difference between x and y
+#'
+#' @param x numeric value
+#' @param y numeric value
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' subtrair(1, 1)
+#'
+subtrair <- function(x, y) {
+  x - y
+}
