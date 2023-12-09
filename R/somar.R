29962447eda87cb6c5b95b5094f49d0f4c4f2a73
jstatlab <jstatlab@gmail.com>
2023-12-09 09:09:21 -0300

    Initial commit

---
+++ R/somar.R
@@ -0,0 +1,14 @@
+#' somar return the sum of x and y
+#'
+#' @param x numeric value
+#' @param y numeric value
+#'
+#' @return x + y
+#' @export
+#'
+#' @examples
+#' somar(1, 1)
+#'
+somar <- function(x, y) {
+  x + y
+}
