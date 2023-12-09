0ac1b435cb0c0ca04c26d4d4cf05b29b745bfd7d
jstatlab <jstatlab@gmail.com>
2023-12-09 09:59:44 -0300

    Criei o arquivo dividir.R

---
+++ R/dividir.R
@@ -0,0 +1,14 @@
+#' dividir return the ratio between x and y
+#'
+#' @param x A numeric value
+#' @param y A numeric value
+#'
+#' @return A numeric value from x / y
+#' @export
+#'
+#' @examples
+#' dividir(x, y)
+#'
+dividir <- function(x, y) {
+  x / y
+}
